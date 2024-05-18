6629d863d0a89c3f236b7478cf917b5d6ab2b53c
happystander <2029189802@qq.com>
2024-05-18 10:00:24 +0800

    5-18 commit nothing change

--- src/main/java/com/medicalpractitioner/contorller/DoctorController.java
+++ src/main/java/com/medicalpractitioner/contorller/DoctorController.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @RestController
 public class DoctorController {
